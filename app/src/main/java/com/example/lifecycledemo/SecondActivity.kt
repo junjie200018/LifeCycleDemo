1427054ca772c700626f8805039257b927a6d60d
ASUS <wongjj-am18@student.tarc.edu.my>
2021-08-04 13:59:42 +0800

    init

---
+++ app/src/main/java/com/example/lifecycledemo/SecondActivity.kt
@@ -0,0 +1,86 @@
+package com.example.lifecycledemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.lifecycledemo.Models.QuestionModel
+
+class SecondActivity : AppCompatActivity() {
+    private lateinit var myModel : QuestionModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        Log.i("Lifecycle", "onCreate")
+
+        val btn : Button = findViewById(R.id.btnAdd)
+        val tv : TextView = findViewById(R.id.tvQuestion)
+        val tvName : TextView = findViewById(R.id.tvName)
+
+        //myModel = QuestionModel()
+
+
+        //myModel = ViewModelProvider(this).get(QuestionModel::class.java) // ViewModelProvider for single view
+        myModel = QuestionModel.getInstance()
+        tv.text = myModel.getQuestion()
+        tvName.text = myModel.name
+
+
+//        if(savedInstanceState != null) {
+//            tv.text = savedInstanceState?.getString("question")
+//
+//        }
+//        btn.setOnClickListener(){
+//            tv.text = generateQuestion()
+//        }
+
+        btn.setOnClickListener(){
+            tv.text = myModel.generateQuestion()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("Lifecycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle", "onDestroy")
+    }
+
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//
+//        var q = findViewById<TextView>(R.id.tvQuestion).text.toString()
+//        outState.putString("question",q)
+//    }
+
+//    fun generateQuestion() : String{
+//        var no1 = (Math.random()*100).toInt()
+//        var no2 = (Math.random()*100).toInt()
+//
+//        return "${no1} + ${no2} = ?"
+//    }
+}
